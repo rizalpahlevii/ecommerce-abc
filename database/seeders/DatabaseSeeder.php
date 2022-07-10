ced309ad883c28fefa309c88fe6dd76f88bfb762
Rizal Pahlevi <mrizalpahlevi372@gmail.com>
2022-07-10 13:03:07 +0700

    update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,6 +16,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
+        User::create([
+            'name' => 'Rizal',
+            'password' => bcrypt('123123'),
+            'email' => 'rizal@mail.com'
+        ]);
         User::factory()->count(10)->create();
         $this->call(ProductsSeeder::class);
         // \App\Models\User::factory(10)->create();
